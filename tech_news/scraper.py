4698ed57b56ba0516981d9d4d94e7bced4daf35f
Nataniel Santos <nathan.santos159@hotmail.com>
2022-06-24 23:33:08 -0300

    feat: Implements fetch function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,19 @@
+import time
+import requests
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    headers = {"user-agent": "Fake user-agent"}
+    try:
+        time.sleep(1)
+        response = requests.get(url, headers=headers, timeout=3)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
