0ec7eea678b19f54d181b15979e63b9fffc5837c
Nataniel Santos <nathan.santos159@hotmail.com>
2022-06-25 21:23:46 -0300

    feat: Implements scrape_noticia function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,6 +2,7 @@ import time
 import requests
 from parsel import Selector
 
+
 # Requisito 1
 def fetch(url):
     headers = {"user-agent": "Fake user-agent"}
@@ -29,9 +30,31 @@ def scrape_next_page_link(html_content):
     next_page_link = selector.css('a.next::attr(href)').get()
     return next_page_link
 
+
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    link = selector.css('link[rel="canonical"]::attr(href)').get()
+    title = selector.css("h1.entry-title::text").get()
+    date = selector.css("li.meta-date::text").get()
+    author = selector.css("span.author a::text").get()
+    comments_count = selector.css("#comments .title-block::text").re(r"\d")
+    category = selector.css("span.label::text").get()
+    summary = selector.css(".entry-content > p:first-of-type *::text").getall()
+    tags = selector.css('.post-tags a[rel="tag"]::text').getall()
+
+    news_info = {
+        "url": link,
+        "title": title,
+        "timestamp": date,
+        "writer": author,
+        "comments_count": int(comments_count or 0),
+        "summary": ''.join(summary),
+        "tags": tags,
+        "category": category,
+    }
+
+    return news_info
 
 
 # Requisito 5
