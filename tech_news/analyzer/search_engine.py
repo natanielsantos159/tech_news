5b41755098192d05c3d2063a767fb009fccf615e
Nataniel Santos <nathan.santos159@hotmail.com>
2022-06-26 16:55:57 -0300

    feat: Implements search_by_date function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,7 @@
 # Requisito 6
 from tech_news.database import search_news
 import re
+import datetime
 
 
 def search_by_title(title):
@@ -10,9 +11,40 @@ def search_by_title(title):
     return response
 
 
+# Source: https://stackoverflow.com/a/16870699/17502226
+def validate_date(date_text):
+    try:
+        datetime.datetime.strptime(date_text, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    validate_date(date)
+
+    month_list = [
+        "janeiro",
+        "fevereiro",
+        "março",
+        "abril",
+        "maio",
+        "junho",
+        "julho",
+        "agosto",
+        "setembro",
+        "outubro",
+        "novembro",
+        "dezembro",
+    ]
+
+    year, month, day = re.findall(r"\d+", date)
+
+    date_str = f"{int(day)} de {month_list[int(month) - 1]} de {year}"
+    results = search_news({"timestamp": date_str})
+
+    response = [(news["title"], news["url"]) for news in results]
+    return response
 
 
 # Requisito 8
