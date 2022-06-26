5e86716f94cd5c619dfc2f85530e3bb341fa32b5
Nataniel Santos <nathan.santos159@hotmail.com>
2022-06-26 19:11:22 -0300

    feat: Implements top_5_news function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,26 @@
 # Requisito 10
+from tech_news.database import find_news
+import functools
+
+
+def compare_popularity(news1, news2):
+    if news1["comments_count"] < news2["comments_count"]:
+        return 1
+    elif news1["comments_count"] > news2["comments_count"]:
+        return -1
+    elif news1["title"] > news2["title"]:
+        return -1
+    elif news1["title"] < news2["title"]:
+        return 1
+    else:
+        return 0
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_list = find_news()
+    news_list.sort(key=functools.cmp_to_key(compare_popularity))
+    response = [(news["title"], news["url"]) for news in news_list[:5]]
+    return response
 
 
 # Requisito 11
